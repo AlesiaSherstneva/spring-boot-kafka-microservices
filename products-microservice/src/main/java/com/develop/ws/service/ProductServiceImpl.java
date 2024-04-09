ecc2871e65ae8a5bfc6ead6cefec4d3ed15cb034
Alesia Sherstneva <alesiasherstneva@gmail.com>
2024-04-09 23:36:01 +0300

    Write service business logic

--- products-microservice/src/main/java/com/develop/ws/service/ProductServiceImpl.java
+++ products-microservice/src/main/java/com/develop/ws/service/ProductServiceImpl.java
@@ -3,10 +3,17 @@ package com.develop.ws.service;
 import com.develop.ws.rest.CreateProductRestModel;
 import org.springframework.stereotype.Service;
 
+import java.util.UUID;
+
 @Service
 public class ProductServiceImpl implements ProductService {
     @Override
     public String createProduct(CreateProductRestModel productRestModel) {
+        String productId = UUID.randomUUID().toString();
+        // Persist product details into a database (mock)
+
+        ProductCreatedEvent productCreatedEvent = new ProductCreatedEvent(productId,
+                productRestModel.getTitle(), productRestModel.getPrice(), productRestModel.getQuantity());
         return null;
     }
 }
