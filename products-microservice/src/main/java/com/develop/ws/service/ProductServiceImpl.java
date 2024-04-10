b9889eea554eaafb5ec6be77174a1fc296f87692
Alesia Sherstneva <alesiasherstneva@gmail.com>
2024-04-10 22:31:11 +0300

    Write producer sending message asynchronously

--- products-microservice/src/main/java/com/develop/ws/service/ProductServiceImpl.java
+++ products-microservice/src/main/java/com/develop/ws/service/ProductServiceImpl.java
@@ -1,12 +1,21 @@
 package com.develop.ws.service;
 
 import com.develop.ws.rest.CreateProductRestModel;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
 
+@Slf4j
 @Service
+@RequiredArgsConstructor
 public class ProductServiceImpl implements ProductService {
+    private final KafkaTemplate<String, ProductCreatedEvent> kafkaTemplate;
+
     @Override
     public String createProduct(CreateProductRestModel productRestModel) {
         String productId = UUID.randomUUID().toString();
@@ -14,6 +23,19 @@ public class ProductServiceImpl implements ProductService {
 
         ProductCreatedEvent productCreatedEvent = new ProductCreatedEvent(productId,
                 productRestModel.getTitle(), productRestModel.getPrice(), productRestModel.getQuantity());
-        return null;
+
+        CompletableFuture<SendResult<String, ProductCreatedEvent>> future =
+                kafkaTemplate.send("product-created-events-topic", productId, productCreatedEvent);
+        future.whenComplete((result, exception) -> {
+            if (exception != null) {
+                log.error("******* Failed to send message: {}", exception.getMessage());
+            } else {
+                log.info("******* Message sent successfully: {}", result.getRecordMetadata());
+            }
+        });
+
+        log.info("******* Returning product id");
+
+        return productId;
     }
 }
