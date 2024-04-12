e7c7f0f82b80201e82377bc44d44a494254dce52
Alesia Sherstneva <alesiasherstneva@gmail.com>
2024-04-12 23:53:29 +0300

    Handle exceptions in sending messages synchronously

---
+++ products-microservice/src/main/java/com/develop/ws/rest/ErrorMessage.java
@@ -0,0 +1,18 @@
+package com.develop.ws.rest;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ErrorMessage {
+    private Date timestamp;
+    private String message;
+    private String details;
+}
