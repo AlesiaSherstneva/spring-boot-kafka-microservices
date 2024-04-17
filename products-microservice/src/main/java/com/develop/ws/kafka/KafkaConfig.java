cf29a6c4102972140462e824d5a83f05b4e7737a
Alesia Sherstneva <alesiasherstneva@gmail.com>
2024-04-18 00:17:47 +0300

    Enable Kafka producer idempotence

--- products-microservice/src/main/java/com/develop/ws/kafka/KafkaConfig.java
+++ products-microservice/src/main/java/com/develop/ws/kafka/KafkaConfig.java
@@ -16,8 +16,10 @@ import java.util.Map;
 import static org.apache.kafka.clients.producer.ProducerConfig.ACKS_CONFIG;
 import static org.apache.kafka.clients.producer.ProducerConfig.BOOTSTRAP_SERVERS_CONFIG;
 import static org.apache.kafka.clients.producer.ProducerConfig.DELIVERY_TIMEOUT_MS_CONFIG;
+import static org.apache.kafka.clients.producer.ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG;
 import static org.apache.kafka.clients.producer.ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG;
 import static org.apache.kafka.clients.producer.ProducerConfig.LINGER_MS_CONFIG;
+import static org.apache.kafka.clients.producer.ProducerConfig.MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION;
 import static org.apache.kafka.clients.producer.ProducerConfig.REQUEST_TIMEOUT_MS_CONFIG;
 import static org.apache.kafka.clients.producer.ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG;
 
@@ -44,6 +46,12 @@ public class KafkaConfig {
     @Value("${spring.kafka.producer.properties.request.timeout.ms}")
     private String requestTimeout;
 
+    @Value("${spring.kafka.producer.properties.enable.idempotence}")
+    private boolean idempotence;
+
+    @Value("${spring.kafka.producer.properties.max.in.flight.requests.per.connection}")
+    private Integer inFlightRequests;
+
     Map<String, Object> producerConfigs() {
         Map<String, Object> config = new HashMap<>();
         config.put(BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
@@ -53,6 +61,9 @@ public class KafkaConfig {
         config.put(DELIVERY_TIMEOUT_MS_CONFIG, deliveryTimeout);
         config.put(LINGER_MS_CONFIG, linger);
         config.put(REQUEST_TIMEOUT_MS_CONFIG, requestTimeout);
+        config.put(ENABLE_IDEMPOTENCE_CONFIG, idempotence);
+        config.put(MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION, inFlightRequests);
+        // config.put(RETRIES_CONFIG, Integer.MAX_VALUE);
 
         return config;
     }
