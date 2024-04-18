e1f0215f411a4d2bbf1de0de34c9b67aa8b19caa
Alesia Sherstneva <alesiasherstneva@gmail.com>
2024-04-19 00:29:21 +0300

    Create email notification microservice

---
+++ email-notification-microservice/src/test/java/com/develop/ws/EmailNotificationMicroserviceApplicationTests.java
@@ -0,0 +1,11 @@
+package com.develop.ws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailNotificationMicroserviceApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
