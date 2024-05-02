446aecca1bb120f75b7b5a77a39ece78f6745c9d
Alesia Sherstneva <alesiasherstneva@gmail.com>
2024-05-03 01:30:31 +0300

    Create RestTemplate bean and inject it to the event handler

--- email-notification-microservice/src/main/java/com/develop/ws/handler/ProductCreatedEventHandler.java
+++ email-notification-microservice/src/main/java/com/develop/ws/handler/ProductCreatedEventHandler.java
@@ -1,15 +1,20 @@
 package com.develop.ws.handler;
 
 import com.develop.ws.ProductCreatedEvent;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaHandler;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
 
 @Slf4j
 @Component
 @KafkaListener(topics = "product-created-events-topic")
+@RequiredArgsConstructor
 public class ProductCreatedEventHandler {
+    private final RestTemplate restTemplate;
+
     @KafkaHandler
     public void handle(ProductCreatedEvent productCreatedEvent) {
         log.info("Received a new event: {}", productCreatedEvent.getTitle());
