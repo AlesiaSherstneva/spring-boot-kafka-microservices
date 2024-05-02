126a48ace733945350cced9b9e54ee655e6d3622
Alesia Sherstneva <alesiasherstneva@gmail.com>
2024-05-03 01:34:10 +0300

    Simulate throwing custom exceptions

--- email-notification-microservice/src/main/java/com/develop/ws/handler/ProductCreatedEventHandler.java
+++ email-notification-microservice/src/main/java/com/develop/ws/handler/ProductCreatedEventHandler.java
@@ -1,11 +1,17 @@
 package com.develop.ws.handler;
 
 import com.develop.ws.ProductCreatedEvent;
+import com.develop.ws.exception.NotRetryableException;
+import com.develop.ws.exception.RetryableException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.kafka.annotation.KafkaHandler;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.HttpServerErrorException;
+import org.springframework.web.client.ResourceAccessException;
 import org.springframework.web.client.RestTemplate;
 
 @Slf4j
@@ -18,5 +24,21 @@ public class ProductCreatedEventHandler {
     @KafkaHandler
     public void handle(ProductCreatedEvent productCreatedEvent) {
         log.info("Received a new event: {}", productCreatedEvent.getTitle());
+
+        // simulate throwing retryable exception
+        String requestUrl = "http://localhost:8082";
+
+        try {
+            ResponseEntity<String> response = restTemplate.exchange(requestUrl, HttpMethod.GET, null, String.class);
+            if (response.getStatusCode().is2xxSuccessful()) {
+                log.info("Received response: {}", response.getBody());
+            }
+        } catch (ResourceAccessException exception) {
+            log.error(exception.getMessage());
+            throw new RetryableException(exception);
+        } catch (HttpServerErrorException exception) {
+            log.error(exception.getMessage());
+            throw new NotRetryableException(exception);
+        }
     }
 }
