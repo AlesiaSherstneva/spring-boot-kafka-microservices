fb702d76256ea8e35700d9e6e516c377bda6b5d5
Alesia Sherstneva <alesiasherstneva@gmail.com>
2024-05-04 18:27:43 +0300

    Add mockservice module for testing exceptions

---
+++ mockservice/src/test/java/com/develop/ws/MockserviceApplicationTests.java
@@ -0,0 +1,11 @@
+package com.develop.ws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockserviceApplicationTests {
+	@Test
+	void contextLoads() {
+	}
+}
