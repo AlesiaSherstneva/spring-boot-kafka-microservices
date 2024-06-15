e67e6bb78cec52bc6d6b992ce12dcc6451e5df57
Alesia Sherstneva <alesiasherstneva@gmail.com>
2024-06-15 23:17:34 +0300

    Configure reading committed messages in consumer

--- 02-deposits-app/withdrawal-service/src/main/java/com/develop/estore/KafkaConsumerConfiguration.java
+++ 02-deposits-app/withdrawal-service/src/main/java/com/develop/estore/KafkaConsumerConfiguration.java
@@ -44,6 +44,8 @@ public class KafkaConsumerConfiguration {
 		config.put(JsonDeserializer.TRUSTED_PACKAGES,
 				environment.getProperty("spring.kafka.consumer.properties.spring.json.trusted.packages"));
 
+		config.put(ConsumerConfig.ISOLATION_LEVEL_CONFIG, environment.getProperty("spring.kafka.consumer.isolation-level"));
+
 		return new DefaultKafkaConsumerFactory<>(config);
 	}
 
