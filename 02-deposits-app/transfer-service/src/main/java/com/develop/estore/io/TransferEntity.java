1a2a39276a17974b2260514677a51779114dcf20
Alesia Sherstneva <alesiasherstneva@gmail.com>
2024-07-10 23:05:06 +0300

    Create transfer entity

---
+++ 02-deposits-app/transfer-service/src/main/java/com/develop/estore/io/TransferEntity.java
@@ -0,0 +1,31 @@
+package com.develop.estore.io;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "transfers")
+@Getter
+@Setter
+public class TransferEntity {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @Column(nullable = false)
+    private String transferId;
+
+    @Column(nullable = false)
+    private String senderId;
+
+    @Column(nullable = false)
+    private String recipientId;
+
+    @Column(nullable = false)
+    private BigDecimal amount;
+}
