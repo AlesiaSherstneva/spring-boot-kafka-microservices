a65b0453e8a84eff6a9294209c49477c26df5251
Alesia Sherstneva <alesiasherstneva@gmail.com>
2024-07-11 20:33:26 +0300

    Create transfer repository

---
+++ 02-deposits-app/transfer-service/src/main/java/com/develop/estore/io/TransferRepository.java
@@ -0,0 +1,7 @@
+package com.develop.estore.io;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TransferRepository extends JpaRepository<TransferEntity, String> {
+
+}
